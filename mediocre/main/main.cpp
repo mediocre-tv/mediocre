46686243306bc0bf72bc96bd9f3dfebc408b7fea
Oliver Broomhall <obroomhall@gmail.com>
2024-11-16 09:57:18 +0000

    Use c++13 threads

--- mediocre/main/main.cpp
+++ mediocre/main/main.cpp
@@ -3,20 +3,34 @@
 #include <mediocre/main/main.hpp>
 #include <mediocre/server/server.hpp>
 #include <thread>
+#include <atomic>
 
 static auto server = mediocre::server::Server(8081);
+
+std::atomic<bool> shutdown_requested = false;
 
 void shutdown() {
     server.shutdown_server();
 }
 
 void signal_handler(const int signal_num) {
-    std::thread thread(shutdown);
-    thread.join();
+    shutdown_requested = true;
 }
 
 int main(int argc, char *argv[]) {
     signal(SIGTERM, signal_handler);
-    server.run_server();
+
+    std::thread server_thread([] {
+        server.run_server();
+    });
+
+    while (!shutdown_requested) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    shutdown();
+
+    server_thread.join();
+
     return EXIT_SUCCESS;
 }
